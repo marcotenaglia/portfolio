cdf16af79e14e4ec9ddc8b8c082e5671ad2af7c8
Marco <marcotenaglia@gmail.com>
2024-01-26 11:43:25 -0300

    links in cards

--- src/App.jsx
+++ src/App.jsx
@@ -9,62 +9,133 @@ function App() {
     <div id="projects" className="projects">
       <h2 className='section-title'>PROJECTS</h2>
       <div className='card-box'>
+
         <div className="card">
-          <h1>E-commerce</h1>
-          <p>Responsive e-commerce with shoping cart</p>
-          <div className='lenguages'>
-            <div>HTML</div>
-            <div>CSS</div>
-            <div>SASS</div>
+          <div>
+            <h1>E-commerce</h1>
+            <p>Responsive e-commerce with shoping cart</p>
+          </div>
+          <div>
+            <div className='lenguages'>
+              <div>HTML</div>
+              <div>CSS</div>
+              <div>SASS</div>
+            </div>
+            <div className='links'>
+              <a target='_blank' className='anchor-tag-link' href="https://github.com/marcotenaglia/e-commerce-js">
+                <svg fill='rgb(219, 226, 232)' class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
+              </a>
+              <a target='blank' className='anchor-tag-link' href="https://marcotenaglia.github.io/e-commerce-js/"><svg class="MuiSvgIcon-root" fill='rgb(219, 226, 232)' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
+            </div>
+            <div>
+              <a href=""></a>
+            </div>
           </div>
         </div>
+
         <div className="card">
-          <h1>To-do List</h1>
-          <p>Checklist for tasks and to-do's</p>
-          <div className='lenguages'>
-            <div>HTML</div>
-            <div>CSS</div>
-            <div>SASS</div>
+          <div>
+            <h1>To-do List</h1>
+            <p>Checklist for tasks and to-do's</p>
+          </div>
+          <div>
+            <div className='lenguages'>
+              <div>HTML</div>
+              <div>CSS</div>
+              <div>SASS</div>
+            </div>
+            <div className='links'>
+              <a target='blank' href="https://github.com/marcotenaglia/todo-list-js">
+                <svg fill='rgb(219, 226, 232)' class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
+              </a>
+              <a target='blank' href="https://marcotenaglia.github.io/todo-list-js/"><svg class="MuiSvgIcon-root" fill='rgb(219, 226, 232)' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
+            </div>
           </div>
         </div>
+
         <div className="card">
-          <h1>Band Web</h1>
-          <p>A band web for content, biography, shows and contact</p>
-          <div className='lenguages'>
-            <div>HTML</div>
-            <div>CSS</div>
-            <div>SASS</div>
+          <div>
+            <h1>Band Web</h1>
+            <p>A band web for content, biography, shows and contact</p>
+          </div>
+          <div>
+            <div className='lenguages'>
+              <div>HTML</div>
+              <div>CSS</div>
+              <div>SASS</div>
+            </div>
+            <div className='links'>
+              <a target='blank' className='anchor-tag-link' href="https://github.com/marcotenaglia/las-aventuras-web">
+                <svg fill='rgb(219, 226, 232)' class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
+              </a>
+              <a target='blank' className='anchor-tag-link' href="https://marcotenaglia.github.io/las-aventuras-web/"><svg class="MuiSvgIcon-root" fill='rgb(219, 226, 232)' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
+            </div>
           </div>
         </div>
+
         <div className="card">
-          <h1>Music Single Web</h1>
-          <p>This was an experimental project for my band. At the end of a gifted fanzine, people would find a QR code that redirected to this exclusive demo.</p>
-          <div className='lenguages'>
-            <div>HTML</div>
-            <div>CSS</div>
-            <div>SASS</div>
+          <div>
+            <h1>Music Single Web</h1>
+            <p>This was an experimental project for my band. At the end of a gifted fanzine, people would find a QR code that redirected to this exclusive demo.</p>
+          </div>
+          <div>
+            <div className='lenguages'>
+              <div>HTML</div>
+              <div>CSS</div>
+              <div>SASS</div>
+            </div>
+            <div className='links'>
+              <a target='blank' href="https://github.com/marcotenaglia/las-aventuras-demo">
+                <svg fill='rgb(219, 226, 232)' class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
+              </a>
+              <a target='blank' href="https://marcotenaglia.github.io/las-aventuras-demo/"><svg class="MuiSvgIcon-root" fill='rgb(219, 226, 232)' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
+            </div>
           </div>
         </div>
+
         <div className="card">
-          <h1> Delivery Receipt Generation</h1>
-          <p>Google Apps Script code that triggers with added rows, converts pertinent information into a delivery receipt PDF and saves it in Google Drive.</p>
-          <div className='lenguages'>
-            <div>GAS</div>
-            <div>Javascript</div>
-            <div>Spreadsheet</div>
-            <div>CSS</div>
-            <div>HTML</div>
+          <div>
+            <h1> Delivery Receipt Generation</h1>
+            <p>Google Apps Script converts pertinent information into a delivery receipt PDF and saves it in Google Drive.</p>
           </div>
+
+          <div>
+            <div className='lenguages'>
+              <div>GAS</div>
+              <div>Javascript</div>
+              <div>Spreadsheet</div>
+              <div>CSS</div>
+              <div>HTML</div>
+            </div>
+            <div className='links'>
+              <a target='blank' href="https://github.com/marcotenaglia/crowntainer-apps-script">
+                <svg fill='rgb(219, 226, 232)' class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
+              </a>
+              <a target='blank' href="https://www.youtube.com/watch?v=PaWglLJ0ps8"><svg class="MuiSvgIcon-root" fill='rgb(219, 226, 232)' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
+            </div>
+          </div>
+          
         </div>
+
         <div className="card">
-          <h1>Budget Generation</h1>
-          <p>A Google Apps Script code that fills a template with updated info from a spreadsheet, converts it into a PDF and saves it in Google Drive </p>
-          <div className='lenguages'>
-            <div>GAS</div>
-            <div>Javascript</div>
-            <div>Spreadsheet</div>
-            <div>CSS</div>
-            <div>HTML</div>
+          <div>
+            <h1>Budget Generation</h1>
+            <p>A Google Apps Script code that fills a template with updated info from a spreadsheet, converts it into a PDF and saves it in Google Drive </p>
+          </div>
+          <div>
+            <div className='lenguages'>
+              <div>GAS</div>
+              <div>Javascript</div>
+              <div>Spreadsheet</div>
+              <div>CSS</div>
+              <div>HTML</div>
+            </div>
+            <div className='links'>
+              <a target='blank' href="https://github.com/marcotenaglia/medios-litoral-apps-script">
+                <svg fill='rgb(219, 226, 232)' class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path></svg>
+              </a>
+              <a target='blank' href="https://www.youtube.com/watch?v=PaWglLJ0ps8"><svg class="MuiSvgIcon-root" fill='rgb(219, 226, 232)' focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg></a>
+            </div>
           </div>
         </div>
 
