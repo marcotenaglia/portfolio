5ff6b2b40b59119bac45b49dd1fa0338e3246479
Marco <marcotenaglia@gmail.com>
2024-02-07 16:07:01 -0300

    commit 2.7 4:06

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const HamburguerMenu = ({ isOpen, toggleMenu, isDarkMode }) => (
-  <div className={`hamburguer-menu ${isOpen ? 'open' : ''} `} onClick={toggleMenu}>
-    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' :  'light'}`}></div>
+  <button className={`hamburguer-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu} role="button" tabIndex={0} >
     <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' : 'light'}`}></div>
     <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' : 'light'}`}></div>
-  </div>
+    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' : 'light'}`}></div>
+  </button>
 );
 
 export default HamburguerMenu;
