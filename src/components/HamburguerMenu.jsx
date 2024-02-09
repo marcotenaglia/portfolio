b0dc01e82ccee432714e6668471e1c73e881bcbd
Marco <marcotenaglia@gmail.com>
2024-02-08 21:49:01 -0300

    habia un error en el ultimo commit, ahora si whiter css

--- src/components/HamburguerMenu.jsx
+++ src/components/HamburguerMenu.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HamburguerMenu = ({ isOpen, toggleMenu, isDarkMode }) => (
   <button className={`hamburguer-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu} role="button" tabIndex={0} >
-    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' : 'light'}`}></div>
-    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' : 'light'}`}></div>
-    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'dark' : 'light'}`}></div>
+    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'light' : 'dark'}`}></div>
+    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'light' : 'dark'}`}></div>
+    <div className={`hamburguer-line hamburguer-line-${isDarkMode ? 'light' : 'dark'}`}></div>
   </button>
 );
 
